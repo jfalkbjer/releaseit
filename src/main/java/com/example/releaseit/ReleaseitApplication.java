5fa897467a3c9c4b57bccef04b0b2adcd46e1a1f
jfalkbjer <jimmy.falkbjer@gmail.com>
2025-01-25 14:50:31 +0100

    fix: test

--- src/main/java/com/example/releaseit/ReleaseitApplication.java
+++ src/main/java/com/example/releaseit/ReleaseitApplication.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * 
- */
 @SpringBootApplication
 public class ReleaseitApplication {
 
