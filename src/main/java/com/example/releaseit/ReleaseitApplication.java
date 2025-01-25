6a6992f92d1fe312281e12a51f55d88432227a33
jfalkbjer <jimmy.falkbjer@gmail.com>
2025-01-25 10:08:17 +0100

    added RestController

--- src/main/java/com/example/releaseit/ReleaseitApplication.java
+++ src/main/java/com/example/releaseit/ReleaseitApplication.java
@@ -1,13 +1,27 @@
 package com.example.releaseit;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ReleaseitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReleaseitApplication.class, args);
+	}
+
+	@RestController
+	public class ReleaseitRestController {
+
+		@GetMapping("/hello")
+		public Map<String, String> sayHelloF() {
+			return Map.of("message", "Hello World!");
+		}
+
 	}
 
 }
