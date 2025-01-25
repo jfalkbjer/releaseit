3760f1f0fe307fd46ae2f64f5f80eba8b4f57cc7
jfalkbjer <jimmy.falkbjer@gmail.com>
2025-01-25 09:54:00 +0100

    Initial commit

---
+++ src/main/java/com/example/releaseit/ReleaseitApplication.java
@@ -0,0 +1,13 @@
+package com.example.releaseit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/releaseit/ReleaseitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.releaseit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
